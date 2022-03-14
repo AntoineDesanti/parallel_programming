72fc1ea4407468a3c528eedc4914c532b60b15c5
AntoineDesanti <antoinedesanti@aol.fr>
2022-03-14 18:20:41 +0100

    params init done

--- project_1/radixsort.c
+++ project_1/radixsort.c
@@ -33,25 +33,56 @@ int* randomFilledTab(int n){
 	return;
 }
 
+bool isNumber(char number[])
+{
+    int i = 0;
+
+    //checking for negative numbers
+    if (number[0] == '-')
+        i = 1;
+    for (; number[i] != 0; i++)
+    {
+        //if (number[i] > '9' || number[i] < '0')
+        if (!isdigit(number[i]))
+            return false;
+    }
+    return true;
+}
+
 int main(int argc, char **argv) {
-	if(argc < 3){
+	if(argc < 4){
 		printf("Needs the following arguments: <value of N> <size of the array> [<input file name>] <output file name> [number of threads, otherwise the by default number]");
 	}
-	bool inputFileUsed = false;
-
-	if(argv[3]){
-		inputFileUsed = true;
-	}
-
+	//BEGIN PARAMS SETUP
+	bool inputFileUsed = true;
 	int n_value = argv[1];
 	int array_size = argv[2];
-	char* input_file_name = argv[3];
-	char* output_file_name = argv[4];
+	char* input_file_name; 
+	char* output_file_name;
 	int number_of_threads = 4;
 
-	if(argv[5]){
-		number_of_threads = argv[5];
+	if(!isNumber(argv[3]) && isNumber(argv[4])){
+		inputFileUsed = false;
 	}
+
+	if(inputFileUsed){
+		input_file_name = argv[3];
+		output_file_name = argv[4];
+		if(argv[5]){
+			number_of_threads = atoi(argv[5]);
+		}
+		// printf("input file used: %s output_file_name:%s number_of_threads:%d",
+		// input_file_name,output_file_name,number_of_threads  );
+	}
+	else{
+		output_file_name = argv[3];
+			if(argv[4]){
+			number_of_threads = atoi(argv[4]);
+		}
+		//printf("NO input file. output_file_name:%s number_of_threads:%d",output_file_name,number_of_threads  );
+	}
+	//END PARAMS SETUP
+
 
 	struct tablo *tab;
 	if(inputFileUsed){
